3ae8adc568cfa413b480221adb90738a85b15123
stasgonik <alaroskael@gmail.com>
2020-12-08 10:57:10 +0200

    start

--- React/app-1/src/App.js
+++ React/app-1/src/App.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
-import AllCommentsComponent from './components/comments/AllCommentsComponent';
-import AllPostsComponent from './components/posts/AllPostsComponent';
+import React, { Component } from "react";
+import AllCommentsComponent from "./components/comments/AllCommentsComponent";
+import AllPostsComponent from "./components/posts/AllPostsComponent";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
 class App extends Component {
-  
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li><Link className="main_link" to={"/posts"}>All Posts</Link></li>
+            <li><Link className="main_link" to={"/comments"}>All Posts</Link></li>
+          </ul>
+          
+          
 
-  render() { 
-    
+          <Switch>
+            <Route path={"/posts"} component={AllPostsComponent}/>
+            <Route path={"/comments"} component={AllCommentsComponent}/>
+          </Switch>
 
-    return (<div>
-      
-      <AllCommentsComponent/>
-      <AllPostsComponent/>
-
-
-    </div>);
+          
+        </div>
+      </Router>
+    );
   }
 }
- 
-export default App;
 
+export default App;
