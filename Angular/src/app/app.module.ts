64972d5ac5c511ee63850a430bf100315422d4c7
stasgonik <alaroskael@gmail.com>
2021-01-19 10:36:18 +0200

    Angular-h1

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
