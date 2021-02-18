052a33cf41af312d87f704fd53d08fb55a641ec7
stasgonik <alaroskael@gmail.com>
2021-02-18 10:52:02 +0200

    Flat

---
+++ NODE/HW1/FlattenDirs/app.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const garbageFolder = path.join(__dirname, "Chaos");
+const normalFolder = path.join(__dirname, "Order");
+
+function flat(garbageDir, normalDir) {
+    fs.readdir(garbageDir, (err, files) => {
+        if (err) {
+            console.log(err);
+            return ;
+        }
+        files.forEach(file => {
+            let fPath = path.join(garbageDir, `${file}`);
+            fs.stat(fPath, (err1, stats) => {
+                if (err1) {
+                    console.log(err1);
+                    return ;
+                }
+                if (stats.isDirectory()) {
+                    flat(fPath, normalDir);
+                } else {
+                    fs.rename(fPath, path.join(normalDir, `${file}`), err2 => {
+                        if (err2) {
+                            console.log(err2);
+                            return ;
+                        }
+                    })
+                }
+            })
+        })
+    })
+}
+
+flat(garbageFolder, normalFolder);
