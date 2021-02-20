fbfcbf8f5e9276e6aa71a2c7a2e7ebff22f07f7d
stasgonik <alaroskael@gmail.com>
2021-02-20 22:53:11 +0200

    Node HW2 - Fix2

--- NODE/HW2/app.js
+++ NODE/HW2/app.js
@@ -8,16 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.set("view engine", ".hbs");
-app.engine(".hbs", expressHBS({
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHBS({
     defaultLayout: false
 }));
-app.set("views", path.join(__dirname, "static"));
+app.set('views', path.join(__dirname, 'static'));
 //=====================Конфигурация придложения==========================
 
 //===================== Авторизация ==========================
+
 app.get('/login', (req, res) => {
     res.render('login');
 });
@@ -28,22 +29,25 @@ app.post('/login', (req, res) => {
             console.log(err);
             return;
         }
+
         let {email, password} = req.body
         const arrayUsers = JSON.parse(data.toString());
         let search = arrayUsers.findIndex(user => {
             return (user.email === email && user.password === password);
         });
 
-        if (search !== -1) {
-            res.redirect(`/users/${search}`);
-        } else {
-            res.redirect("/register");
+        if(search === -1) {
+            res.redirect('/register');
+            return ;
         }
+        res.redirect(`/users/${search}`);
+
     });
 });
 //===================== Авторизация ==========================
 
 //===================== Регистрация ==========================
+
 app.get('/register', (req, res) => {
     res.render('register');
 });
@@ -54,36 +58,41 @@ app.post('/register', (req, res) => {
             console.log(err);
             return;
         }
+
         let {email} = req.body
         const arrayUsers = JSON.parse(data.toString());
         let search = arrayUsers.find(user => {
             return user.email === email;
         });
 
-        if (!search) {
-            arrayUsers.push(req.body);
-            fs.writeFile(path.join(__dirname, 'users.txt'), JSON.stringify(arrayUsers), err1 => {
-                if (err1) {
-                    console.log(err1);
-                    return;
-                }
-
-            });
-            res.redirect("/users");
-        } else {
-            res.redirect("/regError");
+        if (search) {
+            res.redirect('/regError');
+            return ;
         }
+
+        arrayUsers.push(req.body);
+        fs.writeFile(path.join(__dirname, 'users.txt'), JSON.stringify(arrayUsers), err1 => {
+            if (err1) {
+                console.log(err1);
+                return;
+            }
+        });
+        res.redirect('/users');
+
     });
 });
 //===================== Регистрация ==========================
 
 //===================== Ошибка при регистрации ==========================
-app.get("/regError", (req, res) => {
+
+app.get('/regError', (req, res) => {
     res.render('regError');
 });
+
 //===================== Ошибка при регистрации ==========================
 
 //===================== Рендер пользователей ==========================
+
 app.get('/users', (req, res) => {
     fs.readFile(path.join(__dirname, 'users.txt'), (err, data) => {
         if (err) {
@@ -111,5 +120,5 @@ app.get('/users/:userID', (req, res) => {
 //===================== Рендер пользователей ==========================
 
 app.listen(5042, () => {
-    console.log("Starting NODE SERVER, proceed to localhost:5042");
+    console.log('Starting NODE SERVER, proceed to localhost:5042');
 });
