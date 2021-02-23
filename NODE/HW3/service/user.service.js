422ffd962289f9e08d482b257c3649d6ae668480
stasgonik <62423603+stasgonik@users.noreply.github.com>
2021-02-23 18:47:04 +0200

    Update user.service.js

--- NODE/HW3/service/user.service.js
+++ NODE/HW3/service/user.service.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const errorMessage = require('../message/error.message');
 
-const dbPath = path.join(process.cwd(), 'database', 'users.json');
+const dbPath = path.join(process.cwd(), 'dataBase', 'users.json');
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
