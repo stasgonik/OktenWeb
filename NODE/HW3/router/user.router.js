76eb384ad5ca68ec67f94e6444db81c420695528
stasgonik <62423603+stasgonik@users.noreply.github.com>
2021-02-23 18:12:06 +0200

    Update user.router.js

--- NODE/HW3/router/user.router.js
+++ NODE/HW3/router/user.router.js
@@ -5,12 +5,12 @@ const userMiddleware = require('../middleware/user.middleware');
 
 router.get('/', userController.getAllUsers);
 
-router.post('/', userMiddleware.isUserValid, userController.createUser);
+router.post('/', userMiddleware.isUsernameValid, userMiddleware.isPasswordValid, userController.createUser);
 
 router.get('/:userId', userMiddleware.isIdValid, userController.getSingleUser);
 
 router.delete('/:userId', userMiddleware.isIdValid, userController.deleteSingleUser);
 
-// router.get('/findUser', userMiddleware.isUsernameValid, userController.getUserByUsername);
+router.get('/findUser/:username', userMiddleware.isUsernameValid, userController.getUserByUsername);
 
 module.exports = router;
